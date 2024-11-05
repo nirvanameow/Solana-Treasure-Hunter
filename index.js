d6373ef8db30ab79c66177e613045c890a142c0b
Nirvana <168984811+nirvanameow@users.noreply.github.com>
2024-11-05 23:00:48 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const fs = require('fs');
-const readline = require('readline');
 const { Keypair, Connection, clusterApiUrl } = require('@solana/web3.js');
 const { derivePath } = require('ed25519-hd-key');
 const bip39 = require('bip39');
 const { Worker, isMainThread, parentPort, workerData } = require('worker_threads');
 const chalk = require('chalk');
 
-if (isMainThread) {
-  global.seeds = fs.readFileSync('seed-key-words.txt', 'utf8').split('\n');
+const BASE_PATH = '/sol-treasure-data';
 
-  const triedFile = 'logs.json';
+if (isMainThread) {
+  global.seeds = fs.readFileSync(`${BASE_PATH}/seed-key-words.txt`, 'utf8').split('\n');
+  const triedFile = `${BASE_PATH}/logs.json`;
   let triedSet = new Set();
   let triedWallets = {};
   let walletCounter = 0;
@@ -40,32 +40,22 @@ if (isMainThread) {
 
   function saveTriedSet(seedPhrase, pubkey, balance) {
     walletCounter++;
-    triedWallets[walletCounter] = { 
-        seedPhrase: seedPhrase,
-        pubkey: pubkey,
-        balance: balance,
-        timestamp: new Date().toISOString()
-    };
+    triedWallets[walletCounter] = { seedPhrase, pubkey, balance, timestamp: new Date().toISOString() };
     const dataToSave = { triedWallets, walletCounter, lastUpdated: new Date().toISOString() };
     fs.writeFileSync(triedFile, JSON.stringify(dataToSave, null, 2));
     logInfo(`Tried set saved for wallet #${walletCounter}: pubkey: ${pubkey}, balance: ${balance}`);
-   }
+  }
 
   function saveFoundWallet(pubkey, seed, balance) {
+    const foundFile = `${BASE_PATH}/found.json`;
     const foundData = { pubkey, seed, balance };
-    try {
-      if (fs.existsSync('found.json') && fs.readFileSync('found.json', 'utf8').trim() !== '') {
-        const foundWallets = JSON.parse(fs.readFileSync('found.json', 'utf8'));
-        foundWallets.push(foundData);
-        fs.writeFileSync('found.json', JSON.stringify(foundWallets, null, 2));
-        logSuccess(`Found wallet added: ${pubkey}, balance: ${balance}`);
-      } else {
-        fs.writeFileSync('found.json', JSON.stringify([foundData], null, 2));
-        logSuccess(`Found wallet created: ${pubkey}, balance: ${balance}`);
-      }
-    } catch (error) {
-      logError(`Error reading/writing found.json file: ${error.message}`);
+    let foundWallets = [];
+    if (fs.existsSync(foundFile) && fs.readFileSync(foundFile, 'utf8').trim() !== '') {
+      foundWallets = JSON.parse(fs.readFileSync(foundFile, 'utf8'));
     }
+    foundWallets.push(foundData);
+    fs.writeFileSync(foundFile, JSON.stringify(foundWallets, null, 2));
+    logSuccess(`Found wallet added: ${pubkey}, balance: ${balance}`);
   }
 
   function delay(ms) {
@@ -83,20 +73,9 @@ if (isMainThread) {
 
   async function checkInitialization() {
     logInfo('Checking connection to Solana network...');
-    try {
-      const connection = new Connection(clusterApiUrl('mainnet-beta'), {
-        commitment: 'confirmed',
-        fetchOpts: {
-          timeout: 120000 // Timeout de 120 segundos (2 minutos)
-        }
-      });
-      const version = await connection.getVersion();
-      logSuccess(`Connection successfully established. Version: ${version['solana-core']}`);
-    } catch (error) {
-      logError('Error establishing connection to Solana network: ' + error.message);
-      process.exit(1);
-    }
-    logInfo('Initialization successful. Starting main process...');
+    const connection = new Connection(clusterApiUrl('mainnet-beta'), 'confirmed', { fetchOpts: { timeout: 120000 } });
+    const version = await connection.getVersion();
+    logSuccess(`Connection successfully established. Version: ${version['solana-core']}`);
   }
 
   (async () => {
@@ -105,14 +84,7 @@ if (isMainThread) {
 
     const numWorkers = 1;
     for (let i = 0; i < numWorkers; i++) {
-      const worker = new Worker(__filename, {
-        workerData: {
-          seeds: global.seeds,
-          triedSet: Array.from(triedSet),
-          walletCounter: walletCounter,
-          workerId: i + 1
-        }
-      });
+      const worker = new Worker(__filename, { workerData: { seeds: global.seeds, triedSet: Array.from(triedSet), walletCounter, workerId: i + 1 } });
       worker.on('message', (message) => {
         if (message.type === 'saveTriedSet') {
           saveTriedSet(message.seedPhrase, message.pubkey, message.balance);
@@ -120,9 +92,7 @@ if (isMainThread) {
           saveFoundWallet(message.pubkey, message.seedPhrase, message.balance);
         }
       });
-      worker.on('error', (error) => {
-        logError(`Worker ${i + 1} error: ${error.message}`);
-      });
+      worker.on('error', logError);
       worker.on('exit', (code) => {
         if (code !== 0) {
           logError(`Worker ${i + 1} stopped with exit code ${code}`);
@@ -131,9 +101,6 @@ if (isMainThread) {
     }
   })();
 } else {
-  const { Keypair, Connection, clusterApiUrl } = require('@solana/web3.js');
-  const { derivePath } = require('ed25519-hd-key');
-  const bip39 = require('bip39');
   const seeds = workerData.seeds;
   const triedSet = new Set(workerData.triedSet);
   const workerId = workerData.workerId;
@@ -157,26 +124,23 @@ if (isMainThread) {
         const seed = await bip39.mnemonicToSeed(seedPhrase);
         const derivedSeed = derivePath(`m/44'/501'/0'/0'`, seed.toString('hex')).key;
         const keypair = Keypair.fromSeed(derivedSeed);
-
         const connection = new Connection(clusterApiUrl('mainnet-beta'), 'confirmed');
-
         const pubkey = keypair.publicKey.toBase58();
-        console.log(chalk.yellow(`[Worker ${workerId} - INFO] Testing seed: ${seedPhrase} for pubkey: ${pubkey}`));
+        console.log(`[Worker ${workerId} - INFO] Testing seed: ${seedPhrase} for pubkey: ${pubkey}`);
         const balance = await connection.getBalance(keypair.publicKey);
-        console.log(chalk.yellow(`[Worker ${workerId} - INFO] Retrieved balance for wallet ${pubkey}: ${balance}`));
+        console.log(`[Worker ${workerId} - INFO] Retrieved balance for wallet ${pubkey}: ${balance}`);
         if (balance > 0) {
-          console.log(chalk.green(`[Worker ${workerId} - SUCCESS] Found SOL in wallet: ${pubkey} with balance: ${balance}`));
+          console.log(`[Worker ${workerId} - SUCCESS] Found SOL in wallet: ${pubkey} with balance: ${balance}`);
           parentPort.postMessage({ type: 'saveFoundWallet', pubkey, seedPhrase, balance });
-          console.log(chalk.green(`[SUCCESS] Wallet found with balance ${balance} SOL. Halting script.`));
+          console.log(`[SUCCESS] Wallet found with balance ${balance} SOL. Halting script.`);
           process.exit(0);
         } else {
-          console.log(chalk.yellow(`[Worker ${workerId} - INFO] No SOL found in wallet: ${pubkey}`));
+          console.log(`[Worker ${workerId} - INFO] No SOL found in wallet: ${pubkey}`);
         }
         parentPort.postMessage({ type: 'saveTriedSet', seedPhrase, pubkey, balance });
         break;
       } catch (error) {
-        console.error(chalk.red(`[Worker ${workerId} - ERROR] Error checking seed: ${seedPhrase} -> ${error.message}`));
-        console.log(chalk.yellow(`[Worker ${workerId} - INFO] Restarting verification for seed: ${seedPhrase} with randomized delay`));
+        console.error(`[Worker ${workerId} - ERROR] Error checking seed: ${seedPhrase} -> ${error.message}`);
         await delay(5000 + Math.floor(Math.random() * 5000));
       }
     }
