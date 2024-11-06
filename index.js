d013540d0547cdc2dba7debe834421c22878df8a
Nirvana <168984811+nirvanameow@users.noreply.github.com>
2024-11-06 00:33:07 +0000

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const FOUND_FILE = `${BASE_PATH}/found.json`;
 
 // URLs de RPC
 const RPC_URLS = [
-  'https://rpc.ankr.com/solana_devnet/d945aab1993f6de7e5c74cff9dbdbd10893687df038955ba3237436ef3efcd67',
+  'https://delicate-virulent-spree.SOLANA_MAINNET.quiknode.pro/7f9d0d2b071da35895c1905ad243c5506d3abce4',
   'https://api.mainnet-beta.solana.com'
 ];
 
